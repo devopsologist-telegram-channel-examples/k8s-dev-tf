ba67d501a1a477b39a6c43bff9ef2c50dbab0693
Ivanov Pavel <etherlord@etherlord.org>
2024-07-28 15:04:20 +0400

    VM instead of LXC

--- main.tf
+++ main.tf
@@ -11,58 +11,104 @@ provider "proxmox" {
   pm_api_url = "https://proxmox.local:8006/api2/json"
 }
 
-resource "proxmox_lxc" "k8s-master" {
-  target_node  = "proxmox"
-  hostname     = "k8s-master"
-  ostemplate   = "local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
-  unprivileged = true
+resource "proxmox_vm_qemu" "k8s-master" {
+  count = 1
+  name  = "k8s-master"
+  target_node = "proxmox"
+  vmid        = "100"
+  clone       = "debian12.vm"
+  agent       = 1
+  os_type     = "cloud-init"
+  cpu         = "host"
+  vcpus       = 0
+  cores       = 2
+  sockets     = 1
+  memory      = 4096
+  scsihw      = "virtio-scsi-single"
+  bootdisk    = "scsi0"
   onboot = true
-  start = true
 
-  ssh_public_keys = <<-EOT
-    ваш ssh ключ
-  EOT
-
-  cores = 4
-  memory = 4096
-
-  rootfs {
-    storage = "local-lvm"
-    size    = "20G"
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = "local"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size     = "20G"
+          storage  = "local"
+          iothread = true
+        }
+      }
+    }
   }
 
   network {
-    name   = "eth0"
-    bridge = "vmbr0"
-    ip     = "192.168.56.10/24"
-    gw     = "192.168.56.101"
+    model     = "virtio"
+    bridge    = "vmbr0"
+    firewall  = false
+    link_down = false
   }
+
+  boot = "order=scsi0"
+  ipconfig0 = "ip=192.168.0.4/24,gw=192.168.0.3"
+  ciuser    = "terraform"
+  sshkeys   = <<EOF
+    ваш ssh ключ
+  EOF
 }
 
-resource "proxmox_lxc" "k8s-worker" {
-  target_node  = "proxmox"
-  hostname     = "k8s-worker"
-  ostemplate   = "local:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
-  unprivileged = true
+resource "proxmox_vm_qemu" "k8s-worker" {
+  count = 1
+  name  = "k8s-worker"
+  target_node = "proxmox"
+  vmid        = "101"
+  clone       = "debian12.vm"
+  agent       = 1
+  os_type     = "cloud-init"
+  cpu         = "host"
+  vcpus       = 0
+  cores       = 4
+  sockets     = 1
+  memory      = 8192
+  scsihw      = "virtio-scsi-single"
+  bootdisk    = "scsi0"
   onboot = true
-  start = true
 
-  ssh_public_keys = <<-EOT
-    ваш ssh ключ
-  EOT
-
-  cores = 4
-  memory = 8192
-
-  rootfs {
-    storage = "local-lvm"
-    size    = "20G"
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = "local"
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          size     = "20G"
+          storage  = "local"
+          iothread = true
+        }
+      }
+    }
   }
 
   network {
-    name   = "eth0"
-    bridge = "vmbr0"
-    ip     = "192.168.56.11/24"
-    gw     = "192.168.56.101"
+    model     = "virtio"
+    bridge    = "vmbr0"
+    firewall  = false
+    link_down = false
   }
+
+  boot = "order=scsi0"
+  ipconfig0 = "ip=192.168.0.5/24,gw=192.168.0.3"
+  ciuser    = "terraform"
+  sshkeys   = <<EOF
+    ваш ssh ключ
+  EOF
 }
